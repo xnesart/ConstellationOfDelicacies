5b9dad2ab6937ed017bcf5b4a0e354397ba7fb1a
xnesart <xnesart@mail.ru>
2024-03-20 15:51:22 +0300

    AddUser is partially works

--- ConstellationOfDelicacies.Bll/Clients/UserClient.cs
+++ ConstellationOfDelicacies.Bll/Clients/UserClient.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using ConstellationOfDelicacies.Bll.IManager;
+using ConstellationOfDelicacies.Bll.Mapping;
 using ConstellationOfDelicacies.Bll.Models;
 using ConstellationOfDelicacies.Bll.Models.InputModels;
 using ConstellationOfDelicacies.Dal;
@@ -7,18 +8,21 @@ using ConstellationOfDelicacies.Dal.Dtos;
 
 namespace ConstellationOfDelicacies.Bll.Clients;
 
-public class UserClient:IUserClient
+public class UserClient : IUserClient
 {
     private readonly SingletoneStorage _storage;
     private readonly IMapper _mapper;
-    public UserClient(SingletoneStorage storage, IMapper mapper)
+
+    public UserClient()
     {
-        _storage = storage;
-        _mapper = mapper;
+        _storage = SingletoneStorage.GetStorage();
+        IConfigurationProvider config = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfile()); });
+        _mapper = new Mapper(config);
     }
+
     public void AddUser(UsersInputModel model)
     {
-        var dto =_mapper.Map<UsersDto>(model);
+        var dto = _mapper.Map<UsersDto>(model);
         _storage.Storage.Users.Add(dto);
         _storage.Storage.SaveChanges();
     }
@@ -41,7 +45,6 @@ public class UserClient:IUserClient
 
     public List<UsersOutputModel> GetAllUsers()
     {
-        
         var users = _storage.Storage.Users.ToList();
         var usersOutput = _mapper.Map<List<UsersOutputModel>>(users);
 
---
+++ ConstellationOfDelicacies.Bll/Models/InputModels/RolesInputModel.cs
@@ -0,0 +1,7 @@
+namespace ConstellationOfDelicacies.Bll.Models.InputModels;
+
+public class RolesInputModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+}
