a65ee1129b749664ab32df60aac3e9b16cff9bd9
kihoii <kihikolee@gmail.com>
2024-03-22 21:24:17 +0300

    kinda working auth

---
+++ ConstellationOfDelicacies.Bll/Models/InputModels/LoginInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConstellationOfDelicacies.Bll.Models.InputModels
+{
+    public class LoginInputModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- ConstellationOfDelicacies.Web/Program.cs
+++ ConstellationOfDelicacies.Web/Program.cs
@@ -37,6 +37,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
