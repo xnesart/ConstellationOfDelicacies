37a1129b0fd5d9f5bb650245bbc8add953266c64
xnesart <xnesart@mail.ru>
2024-03-26 01:32:51 +0300

    login is partly works

--- ConstellationOfDelicacies.Bll/Models/OutputModels/UsersOutputModel.cs
+++ ConstellationOfDelicacies.Bll/Models/OutputModels/UsersOutputModel.cs
@@ -5,6 +5,7 @@ public class UsersOutputModel
     public int Id { get; set; }
     public ICollection<ProfilesOutputModel> Profiles { get; set; }
     public ICollection<TasksOutputModel> Task { get; set; }
+    public RolesOutputModel? Role { get; set; }
     public string FirstName { get; set; }
     public string? MiddleName { get; set; }
     public string LastName { get; set; }
