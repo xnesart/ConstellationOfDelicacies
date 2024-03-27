b3a8343e244fac67b7e4f95a6f4444ba01b49cdd
kihoii <kihikolee@gmail.com>
2024-03-27 18:20:02 +0300

    redo setting dtos

--- ConstellationOfDelicacies.Bll/UserMock.cs
+++ ConstellationOfDelicacies.Bll/UserMock.cs
@@ -1,48 +1,48 @@
-﻿using ConstellationOfDelicacies.Bll.Clients;
-using ConstellationOfDelicacies.Bll.Models.InputModels;
+﻿//using ConstellationOfDelicacies.Bll.Clients;
+//using ConstellationOfDelicacies.Bll.Models.InputModels;
 
-namespace ConstellationOfDelicacies.Bll
-{
-    public class UserMock: IOrderClient
-    {
-        private List<OrderInputModel> _orders; 
+//namespace ConstellationOfDelicacies.Bll
+//{
+//    public class UserMock: IOrderClient
+//    {
+//        private List<OrderInputModel> _orders; 
 
-        public UserMock()
-        {
-            _orders = new List<OrderInputModel>()
-            {
-                new OrderInputModel()
-                {
-                    UserId = 1,
-                    OrderDate = DateTime.Now,
-                    Comment = "qqq",
-                    Address = "www",
-                    NumberOfPersons = 3
-                },
-                new OrderInputModel()
-                {
-                    UserId = 47,
-                    OrderDate = DateTime.Now,
-                    Comment = "123",
-                    Address = "456",
-                    NumberOfPersons = 1
-                }
-            };
-        }   
+//        public UserMock()
+//        {
+//            _orders = new List<OrderInputModel>()
+//            {
+//                new OrderInputModel()
+//                {
+//                    UserId = 1,
+//                    OrderDate = DateTime.Now,
+//                    Comment = "qqq",
+//                    Address = "www",
+//                    NumberOfPersons = 3
+//                },
+//                new OrderInputModel()
+//                {
+//                    UserId = 47,
+//                    OrderDate = DateTime.Now,
+//                    Comment = "123",
+//                    Address = "456",
+//                    NumberOfPersons = 1
+//                }
+//            };
+//        }   
 
-        public void AddUserOrder(OrderInputModel order)
-        {
-            _orders.Add(order);
-        }
+//        public void AddUserOrder(OrderInputModel order)
+//        {
+//            _orders.Add(order);
+//        }
 
-        public void DeleteUserOrder(int orderId)
-        {
-            throw new NotImplementedException();
-        }
+//        public void DeleteUserOrder(int orderId)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public void UpdateUserOrder(OrderInputModel order)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public void UpdateUserOrder(OrderInputModel order)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
