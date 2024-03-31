01a3b6da1fa8e3ef2df7e45dbd9df4a03e8b8234
kihoii <kihikolee@gmail.com>
2024-04-01 00:18:58 +0300

    user + reg input model validation fixed

--- ConstellationOfDelicacies.Bll/Models/InputModels/RegistrationInputModel.cs
+++ ConstellationOfDelicacies.Bll/Models/InputModels/RegistrationInputModel.cs
@@ -6,11 +6,14 @@ public class RegistrationInputModel
 {
     
     [Required]
+    [MaxLength(50)]
     public string Name { get; set; }
 
+    [MaxLength(50)]
     public string MiddleName { get; set; }
 
     [Required]
+    [MaxLength(50)]
     public string LastName { get; set; }
     
     [Required]
@@ -19,6 +22,7 @@ public class RegistrationInputModel
     public string Phone { get; set; }
 
     [Required]
+    [MinLength(4)]
     public string Password { get; set; }
     
     public RolesInputModel Role { get; set; }
